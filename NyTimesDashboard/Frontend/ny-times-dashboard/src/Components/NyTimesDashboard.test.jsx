29eae87d54dd263e24d72c2020f4420416c89ab4
iustinherciu2000 <tuq84828@temple.edu>
2024-11-20 12:09:30 -0500

    Update NyTimesDashboard.test.jsx

--- NyTimesDashboard/Frontend/ny-times-dashboard/src/Components/NyTimesDashboard.test.jsx
+++ NyTimesDashboard/Frontend/ny-times-dashboard/src/Components/NyTimesDashboard.test.jsx
@@ -4,7 +4,14 @@ import NyTimesDashboard from "./NyTimesDashboard";
 import { screen, render } from "@testing-library/react";
 
 test("test for mock api", async () => {
+
     render(<NyTimesDashboard />);
-    const element = await screen.findByRole("listitem");
+    const element = await screen.findByRole("heading");
     expect(element).toBeInTheDocument();
-})
+});
+
+test("display loading indicator while fetching data", () => {
+    render(<NyTimesDashboard />);
+    const loadingElement = screen.queryByText(/loading/i);
+    expect(loadingElement).toBeInTheDocument();
+});
